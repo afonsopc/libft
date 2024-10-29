8c8d4aa7b7721ca56235c1fd611fb94d4063ea2a
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2024-10-29 21:32:43 +0000

    Make strjoin

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: afpachec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 23:56:49 by afpachec          #+#    #+#             */
-/*   Updated: 2024/10/29 19:52:17 by afpachec         ###   ########.fr       */
+/*   Updated: 2024/10/29 21:27:16 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,6 @@ int		ft_atoi(const char *str);
 char	*ft_strdup(const char *s1);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
