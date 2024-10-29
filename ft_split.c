489729176afb3cfa9616c48d5177ab2487bfbcf5
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2024-10-29 23:42:04 +0000

    Make split

---
+++ ft_split.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afpachec <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 21:44:09 by afpachec          #+#    #+#             */
+/*   Updated: 2024/10/29 23:40:35 by afpachec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+static void	free_all(char **strs, size_t index)
+{
+	while (index-- > 0)
+		free(strs[index]);
+	free(strs);
+}
+
+int	count_items(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s && *s == c)
+		s++;
+	while (*s)
+	{
+		while (*s && *s != c)
+			s++;
+		while (*s && *s == c)
+			s++;
+		i++;
+	}
+	return (i);
+}
+
+int	split(char const *s, char c, char **strs)
+{
+	size_t		i;
+	const char	*start;
+
+	i = 0;
+	while (*s && *s == c)
+		s++;
+	while (*s)
+	{
+		start = s;
+		while (*s && *s != c)
+			s++;
+		strs[i] = malloc(s - start + 1);
+		if (!strs[i])
+		{
+			free_all(strs, i);
+			return (0);
+		}
+		ft_strlcpy(strs[i], start, s - start + 1);
+		while (*s && *s == c)
+			s++;
+		i++;
+	}
+	strs[i] = NULL;
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char		**strs;
+
+	if (!s)
+		return (NULL);
+	strs = malloc(count_items(s, c) + 1);
+	if (!strs)
+		return (NULL);
+	if (!split(s, c, strs))
+		return (NULL);
+	return (strs);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: afpachec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 23:56:49 by afpachec          #+#    #+#             */
-/*   Updated: 2024/10/29 21:41:18 by afpachec         ###   ########.fr       */
+/*   Updated: 2024/10/29 23:41:56 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
