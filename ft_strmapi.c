b34bbc98b1803dd7091f4178721218a59d3b136b
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2024-10-30 12:28:03 +0000

    Make strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afpachec <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 12:06:03 by afpachec          #+#    #+#             */
+/*   Updated: 2024/10/30 12:27:10 by afpachec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	str = ft_strdup(s);
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (str[++i])
+		str[i] = f(i, str[i]);
+	return (str);
+}
