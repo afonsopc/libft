a725c385eb11e1f66b4530a7e97b9c8b2a0b0769
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2024-10-31 14:18:00 +0000

    Fix

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,31 +6,17 @@
 /*   By: afpachec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 14:04:12 by afpachec          #+#    #+#             */
-/*   Updated: 2024/10/30 14:44:15 by afpachec         ###   ########.fr       */
+/*   Updated: 2024/10/31 14:03:30 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	memlen(void *mem)
-{
-	size_t			count;
-	unsigned char	*ptr;
-
-	if (!mem)
-		return (sizeof(unsigned char *));
-	count = 0;
-	ptr = (unsigned char *)mem;
-	while (ptr[count])
-		count++;
-	return (count * sizeof(unsigned char *));
-}
-
 t_list	*ft_lstnew(void *content)
 {
 	t_list	*lst;
 
-	lst = malloc(memlen(content) + sizeof(t_list *));
+	lst = malloc(sizeof(t_list));
 	if (!lst)
 		return (NULL);
 	lst->content = content;
