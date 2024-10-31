2a0e8db737374003d889c9e9626a50661598902a
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2024-10-31 19:59:09 +0000

    Fix lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: afpachec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 17:22:33 by afpachec          #+#    #+#             */
-/*   Updated: 2024/10/30 18:51:56 by afpachec         ###   ########.fr       */
+/*   Updated: 2024/10/31 19:57:23 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	**new;
+	t_list	*new;
 	t_list	*current;
 
-	new = malloc(ft_lstsize(lst) * sizeof(t_list *));
-	if (!new)
-		return (NULL);
+	new = NULL;
 	while (lst)
 	{
 		current = ft_lstnew(f(lst->content));
 		if (!current)
 		{
-			ft_lstclear(new, del);
+			ft_lstclear(&new, del);
 			return (NULL);
 		}
-		ft_lstadd_back(new, current);
+		ft_lstadd_back(&new, current);
 		lst = lst->next;
 	}
-	return (new[0]);
+	return (new);
 }
