893db3f684b48c19a633e30cbc7e5f1b103b944f
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2024-10-31 16:03:12 +0000

    Fix

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: afpachec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:16:36 by afpachec          #+#    #+#             */
-/*   Updated: 2024/10/30 16:59:07 by afpachec         ###   ########.fr       */
+/*   Updated: 2024/10/31 15:17:28 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*curr;
 
-	if (!new)
+	if (!lst || !new)
 		return ;
-	if (!lst[0])
+	if (!*lst)
 	{
-		lst[0] = new;
+		*lst = new;
 		return ;
 	}
 	curr = lst[0];
-	new->next = NULL;
 	while (curr->next)
 		curr = curr->next;
 	curr->next = new;
+	new->next = NULL;
 }
